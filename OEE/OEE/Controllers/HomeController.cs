e0ccc39e39406b4196bbf8d64e44c44135880782
anhdaihuong <bamboo2711@gmail.com>
2022-01-02 19:16:51 +0700

    no message

---
+++ OEE/OEE/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OEE.Models;
+
+namespace OEE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private OEEDataContext db = new OEEDataContext();
+        public ActionResult Index()
+        {
+            var lst = db.spGetData().ToList();
+            if (lst.Count == 0)
+            {
+                return RedirectToAction("Detail", "Home");
+            }
+            return View(lst);
+        }
+
+        public ActionResult Detail()
+        {
+            var lst1 = db.spLCDFull().ToList();
+            if (lst1.Count == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(lst1);
+        }
+        public ActionResult getData()
+        {
+            var query = db.spLCDFull().ToList()
+                .GroupBy(t => t.COT1)
+                .Select(g => new
+                {
+                    name = g.Key,
+                    count = g.Sum(a => Convert.ToInt32(a.COT9BD)),
+                    text = g.Sum(b => Convert.ToInt32(b.COT10BD)),
+                    color = g.FirstOrDefault().MAUBD,
+                    namedn9 = g.Single().DNCOT9BD,
+                    namedn10 = g.Single().DNCOT10BD
+
+                }).ToList();
+            query.RemoveAll(r => r.count == 0);
+            return Json(query, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ OEE/OEE/Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OEE.Models;
+using System.Data.Entity;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+
+namespace OEE.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        private OEEDataContext db = new OEEDataContext();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            var username = fc["txtUSERNAME"];
+            var pass = fc["txtPASS"];
+            if(string.IsNullOrEmpty(username))
+            {
+                ViewBag.ThongBao = "Phải nhập tên đăng nhập";
+            }
+            else if(string.IsNullOrEmpty(pass))
+            {
+                ViewBag.ThongBao = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                USER dn = db.USERS.SingleOrDefault(n => n.USERNAME == username);
+              
+                if (dn != null)
+                {
+                    if (GiaiMaDL(dn.PASS) == pass)
+                    {
+                        Session["Taikhoan"] = dn;
+                        return RedirectToAction("Index", "Home");
+                    }    
+                    else
+                    {
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                        return View();
+                    }    
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        private string GiaiMaDL(string str)
+        {
+            double dLen = str.Length;
+            string sTam = "";
+            for (int i = 1; i <= dLen; i++)
+                sTam += Strings.ChrW((Strings.AscW(Strings.Mid(str, i, 1)) / 2) - 354).ToString();
+            return sTam;
+        
+        }
+    }
+}
